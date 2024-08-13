bafc641d75831cd7c02063263dbfb3a4bbebd371
Walid Hawari <walidhawari95@gmail.com>
2024-08-13 19:15:43 +0300

    [FEAT]: social links profile

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
         inter: ["Inter", "sans-serif"],
       },
     },
+    colors: {
+      "zodiac-blue": {
+        100: "rgb(221, 237, 252)",
+        500: "rgb(69, 148, 236)",
+        600: "rgb(48, 118, 224)",
+        700: "rgb(39, 98, 206)",
+        950: "rgb(20, 33, 61)",
+      },
+    },
   },
   plugins: [],
 };
